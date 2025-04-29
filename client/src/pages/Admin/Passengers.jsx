192c7ea5e0d31cede1bfed0d15835b552d163d2a
yohandesilva <yydsilva@students.nsbm.ac.lk>
2025-04-29 19:27:33 +0530

    Passenger Section Done

---
+++ client/src/pages/Admin/Passengers.jsx
@@ -0,0 +1,11 @@
+import Passenger from "../../components/Admin/PassengerBooking"
+
+function Passengers() {
+    return (
+      <div>
+            <Passenger />
+      </div>
+    )
+  }
+  
+export default Passengers
